0a74522ea7d09e773894106b7f6cf02b5b2f74b5
Karan Gupta <kg042632@gmail.com>
2022-01-12 14:16:57 +0530

    Moving Player With Buttons

---
+++ Assets/Mover.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] float moveSpeed = 0.01f;
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xValue = Input.GetAxis("Horizontal") * Time.deltaTime *moveSpeed;
+        float zValue = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+        transform.Translate(xValue,0,zValue);
+    }
+}
